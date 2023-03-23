4e5a4adebc8a7732c6246325ad80231e20517ec7
AleksandrTurlakov <aturlakov@ukr.net>
2023-03-23 20:31:23 +0200

    add and fix authControllers.test.js

--- __tests__/authControllers.test.js
+++ __tests__/authControllers.test.js
@@ -7,8 +7,9 @@ const { DB_HOST, PORT } = process.env;
 
 mongoose.set("strictQuery", true);
 
-describe("test login controller", () => {
+describe("test auth controllers", () => {
   let server;
+  let token;
   beforeAll(async () => {
     await mongoose.connect(DB_HOST, {
       useNewUrlParser: true,
@@ -24,10 +25,21 @@ describe("test login controller", () => {
     server.close();
   });
 
-  test("login should return status 200", async () => {
+  test("register should return status 201", async () => {
+    const response = await request(server)
+      .post("/api/auth/register")
+      .send({ email: "alex@com.ua", password: "123456" });
+    expect(response.status).toBe(201);
+    expect(typeof response.body.user).toBe("object");
+    expect(typeof response.body.user.email).toBe("string");
+    expect(typeof response.body.user.subscription).toBe("string");
+  });
+
+  it("login should return status 200", async () => {
     const response = await request(server)
       .post("/api/auth/login")
       .send({ email: "alex@com.ua", password: "123456" });
+    token = response.body.token;
     expect(response.status).toBe(200);
     expect(response.body.token).toBeTruthy();
     expect(typeof response.body.user).toBe("object");
@@ -38,5 +50,13 @@ describe("test login controller", () => {
   it("login should return status 400 if email or password is missing", async () => {
     const response = await request(server).post("/api/auth/login").send();
     expect(response.status).toBe(400);
+  });
+
+  it("logout should return status 204", async () => {
+    const response = await request(server)
+      .post("/api/auth/logout")
+      .set("Authorization", `Bearer ${token}`);
+    expect(response.status).toBe(204);
+    expect(response.body).toStrictEqual({});
   });
 });
